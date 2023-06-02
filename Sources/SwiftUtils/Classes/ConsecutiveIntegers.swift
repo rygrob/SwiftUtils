a2fff612abd606b9a3dee6d1dcc02c37b3c0da4c
Ryan Robinson <rygrob@gmail.com>
2023-06-02 11:40:54 -0700

    Add consecutive integers.

---
+++ Sources/SwiftUtils/Classes/ConsecutiveIntegers.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ryan Robinson on 6/2/23.
+//
+
+import Foundation
+
+public class ConsecutiveIntegers {
+    public static func strings(from min: Int, through max: Int) -> [String] {
+        guard min < max else { return [] }
+        
+        var strings: [String] = []
+        
+        var num = min
+        
+        while num <= max {
+            strings.append(String(num))
+            num += 1
+        }
+        
+        return strings
+    }
+}
