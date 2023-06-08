47d4bf3ea37cad7cdd3960119372d954009e5446
Ryan Robinson <rygrob@gmail.com>
2023-06-08 10:47:01 -0700

    Add translation function.

--- Sources/SwiftUtils/Extensions/CGPoint+Extras.swift
+++ Sources/SwiftUtils/Extensions/CGPoint+Extras.swift
@@ -11,4 +11,8 @@ public extension CGPoint {
     func distance(to other: CGPoint) -> CGFloat {
         return sqrt(pow(self.x - other.x, 2) + pow(self.y - other.y, 2))
     }
+    
+    func translation(from other: CGPoint) -> CGPoint {
+        return .init(x: self.x - other.x, y: self.y - other.y)
+    }
 }
