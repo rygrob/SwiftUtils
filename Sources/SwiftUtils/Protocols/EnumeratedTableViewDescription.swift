15b7fd641326713663267f585ce51c5498d80e8a
Ryan Robinson <rygrob@gmail.com>
2023-06-15 14:47:23 -0700

    Add a description protocol for UITableView.

---
+++ Sources/SwiftUtils/Protocols/EnumeratedTableViewDescription.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+protocol EnumeratedTableViewDescription: RawRepresentable<Int>, CaseIterable {
+    static func numberOfSections() -> Int
+    static func title(for section: Int) -> String?
+    static func numberOfRows(in section: Int) -> Int
+    static func item(for section: Int, and row: Int) -> (any CaseIterable)?
+}
+
+extension EnumeratedTableViewDescription {
+    static func numberOfSections() -> Int {
+        return Self.allCases.count
+    }
+}
