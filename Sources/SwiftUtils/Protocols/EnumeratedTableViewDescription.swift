e963d602020a5ab388ac6877e2edbe483680fd41
Ryan Robinson <rygrob@gmail.com>
2023-06-15 14:54:26 -0700

    Make things public.

--- Sources/SwiftUtils/Protocols/EnumeratedTableViewDescription.swift
+++ Sources/SwiftUtils/Protocols/EnumeratedTableViewDescription.swift
@@ -1,13 +1,13 @@
 import Foundation
 
-protocol EnumeratedTableViewDescription: RawRepresentable<Int>, CaseIterable {
-    static func numberOfSections() -> Int
-    static func title(for section: Int) -> String?
-    static func numberOfRows(in section: Int) -> Int
-    static func item(for section: Int, and row: Int) -> (any CaseIterable)?
+public protocol EnumeratedTableViewDescription: RawRepresentable<Int>, CaseIterable {
+    public static func numberOfSections() -> Int
+    public static func title(for section: Int) -> String?
+    public static func numberOfRows(in section: Int) -> Int
+    public static func item(for section: Int, and row: Int) -> (any CaseIterable)?
 }
 
-extension EnumeratedTableViewDescription {
+public extension EnumeratedTableViewDescription {
     static func numberOfSections() -> Int {
         return Self.allCases.count
     }
